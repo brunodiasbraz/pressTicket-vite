9d2d1d95517f098e237f212f6925e04cf96cf124
Bruno Braz <brunobraz@cercred.com.br>
2025-03-20 09:30:44 -0300

    feat: atualizar dependências do projeto, refatorar renderização do App e adicionar funcionalidade de carregamento

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,18 +1,19 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import React from "react";
+import ReactDOM from "react-dom";
+import { initializeLoading } from "./utils/showLoading";
 
 import App from "./App";
 
-// ReactDOM.render(
-// 	<CssBaseline>
-// 		<App />
-// 	</CssBaseline>,
-// 	document.getElementById("root")
-// );
+const finishLoading = initializeLoading();
 
-createRoot(document.getElementById("root")).render(
-  <CssBaseline>
-    <App />
-  </CssBaseline>
+ReactDOM.render(
+    <CssBaseline>
+        <App />
+    </CssBaseline>,
+    document.getElementById("root"),
+    () => {
+        // Aumentado o tempo de espera para garantir que o app esteja carregado
+        setTimeout(finishLoading, 1000);
+    }
 );
